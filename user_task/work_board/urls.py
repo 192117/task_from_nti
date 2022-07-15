845b96ec36abcdb97b9404214726801adbc1cef0
Kokoc <noti1996@gmail.com>
2022-07-15 06:52:58 +0300

    Start project.

---
+++ user_task/work_board/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+
+urlpatterns = [
+    path('task/all/',
+    path('task/my/',
+    path('task/delete/<str:name>/',
+    path('task/create/',
+    path('task/detail/<str:name>/',
+    path('registr/',
+    path('login/',
+]
